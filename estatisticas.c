84916d2722e196711898a914cd557d816c63b723
PedroFonseca028 <pedro.henrique.nas@hotmail.com>
2024-04-29 20:10:35 -0300

    Feito alterações no codigo para incluir a mediana

--- estatisticas.c
+++ estatisticas.c
@@ -1,55 +1,11 @@
 #include <stdio.h>
-
-
-void encontrarModa(int array[], int tamanho) {
-    int frequencia[tamanho]; // Para armazenar a frequência de cada elemento
-    int moda[tamanho]; 
-    int maxFrequencia = 0;
-    int numModas = 0;
-
-    // Inicializa os arrays com zeros
-    for (int i = 0; i < tamanho; i++) {
-        frequencia[i] = 0;
-        moda[i] = 0;
-    }
-
-    // Calcula a frequência de cada elemento
-    for (int i = 0; i < tamanho; i++) {
-        frequencia[array[i]]++; // Incrementa a frequência do número lido
-    }
-
-    // Encontra a maior frequência
-    for (int i = 0; i < tamanho; i++) {
-        if (frequencia[i] > maxFrequencia) {
-            maxFrequencia = frequencia[i];
-        }
-    }
-
-    // Encontra todos os números com a maior frequência
-    for (int i = 0; i < tamanho; i++) {
-        if (frequencia[array[i]] == maxFrequencia) {
-            // Verifica se o número já não está na lista de modas
-            int j;
-            for (j = 0; j < numModas; j++) {
-                if (moda[j] == array[i]) {
-                    break;
-                }
-            }
-            if (j == numModas) { // Se não encontrado na lista, adiciona
-                moda[numModas++] = array[i];
-            }
-        }
-    }
-
-    // Exibe o resultado das modas
-    for (int i = 0; i < numModas; i++) {
-        printf("Moda: %d \n", moda[i]);
-    }
-}
-
+#include <stdlib.h>
+#include <string.h>
+#include "func_estatisticas.c"
 
 int main() {
     int tamanho;
+    float mediana = 0, aux = 0;
     
     // Determina o tamanho do array
     printf("Insira o tamanho do array: ");
@@ -73,16 +29,13 @@ int main() {
     for (int i = 0; i < tamanho; i++) {
         soma += array[i];
     }
-
     // Calculando a média como um float
     float media = (float)soma / tamanho;
-
     // Exibe o resultado da media
     printf("A media dos numeros : %.2f\n", media);
 
     encontrarModa(array, tamanho);
+
+    calcularMediana(array, tamanho);
     return 0;
-
-
-
 }
